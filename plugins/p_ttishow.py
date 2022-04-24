aba844bd81bf0007960350b7ce9919d78727635d
Hellogooys <99305409+Hellogooys@users.noreply.github.com>
2022-04-24 12:11:02 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -58,10 +58,10 @@ async def save_group(bot, message):
                     except:
                         pass
                 temp.MELCOW['welcome'] = await message.reply_video(
-                video="https://telegra.ph/file/922ba803c3e701590dc97.mp4",
+                video=("https://telegra.ph/file/922ba803c3e701590dc97.mp4"),
                 caption=f"<b>💖 ʜᴇʟʟᴏ ᴍʏ ғʀɪᴇɴᴅ {u.mention},</b> <b>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ</b> <b>{message.chat.title} 💖</b>",
-                reply_markup=InlineKeyboardMarkup(buttons)
-                )
+                reply_markup=InlineKeyboardMarkup(buttons))
+                
               
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
